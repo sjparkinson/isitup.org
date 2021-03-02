8525519e7d2361bdaf7a2be1aedc20e320098c26
Samuel Parkinson <samuel.parkinson@hey.com>
2021-03-02 22:57:52 +0000

    Tidy up

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ApoControllerTest extends WebTestCase
+class ApiControllerTest extends WebTestCase
 {
     /**
      * @dataProvider validWebsites
