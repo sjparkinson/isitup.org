ae8d1b4813cebd2b972f2fe9b3d5f21a69306452
Samuel Parkinson <samuel.parkinson@hey.com>
2021-06-12 10:05:56 +0000

    Refactor IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,33 +13,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 final class IndexController extends AbstractController
 {
-    #[Route('/', name: 'root')]
-    public function index(Request $request): Response
+    #[Route('/', methods: ['GET', 'HEAD'])]
+    public function index(): Response
     {
-        if (Request::METHOD_POST == $request->getMethod()) {
-            $website = $request->request->get('website') ?? 'duckduckgo.com';
-            $website = strtolower(preg_replace('/^[ \s]+|[ \s]+$|http(s)?:\/\/|\/(.*)/i', '', $website));
-
-            return $this->redirectToRoute(
-                'app_index_check',
-                ['website' => $website],
-                Response::HTTP_SEE_OTHER
-            );
-        }
-
         return $this->render('index.html.twig', ['website' => 'duckduckgo.com']);
     }
 
-    #[Route('/{website}', requirements: ['website' => '[^/]+'], methods: ['GET', 'HEAD'])]
-    public function check(WebsiteStatusService $websiteStatusService, Request $request, ?string $website): Response
+    #[Route('/', methods: ['POST'])]
+    public function indexSubmit(Request $request): Response
     {
-        $website = $request->query->get('website', $website);
+        $website = $request->request->get('website', 'duckduckgo.com');
+        $website = strtolower(preg_replace('/^[ \s]+|[ \s]+$|http(s)?:\/\/|\/(.*)/i', '', $website));
 
-        // We need a website to check, someone has probably visited /check directly.
-        if (!$website) {
-            throw $this->createNotFoundException();
-        }
+        return $this->redirectToRoute(
+            'app_index_check',
+            ['website' => $website],
+            Response::HTTP_SEE_OTHER
+        );
+    }
 
+    #[Route('/{website}', requirements: ['website' => '[^/]+'], methods: ['GET', 'HEAD'])]
+    public function check(WebsiteStatusService $websiteStatusService, Request $request, string $website): Response
+    {
         try {
             $response = $websiteStatusService->getStatus($website);
         } catch (InvalidWebsiteException) {
