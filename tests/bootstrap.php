fd25bafc627e353874a456afb214fb8a08f2a043
Samuel Parkinson <samuel.parkinson@hey.com>
2021-12-08 21:50:02 +0000

    Ignore Psalm error for missing file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,7 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    /** @psalm-suppress MissingFile */
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
